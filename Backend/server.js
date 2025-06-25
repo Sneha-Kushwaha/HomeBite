78031125eb46c7e91a95ce03678fa6f88ac76966
Sneha Kushwaha <snehakushwaha7394@gmail.com>
2025-06-25 23:24:01 +0530

    api test login/registerd

--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,8 @@ import express from "express"
 import cors from "cors"
 import { connectDB } from "./config/db.js"
 import foodRouter from "./routes/foodRoute.js"
+import userRouter from "./routes/userRoute.js";
+import 'dotenv/config'
 
 
 
@@ -19,12 +21,15 @@ connectDB();
 // api endpoints
 app.use("/api/food",foodRouter);
 app.use("/images", express.static('uploads'))
+app.use("/api/user",userRouter)
 
-app.get("/",(re, res)=> {
+
+app.get("/", (req, res) => {
     res.send("API Working")
 })
 
+
 app.listen(port,()=>{
-    console.log(`Server Started on http://locathost:${port}`)
+    console.log(`Server Started on http://localhost:${port}`)
 })
 
